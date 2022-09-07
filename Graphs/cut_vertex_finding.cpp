b2708b7f882eb1c004eb1f35d7a52bc9e5dd2f46
Kirill Lukoyanov <112931242+K1r1llLukoyanov@users.noreply.github.com>
2022-09-07 19:52:50 +0300

    Update cut_vertex_finding.cpp

--- Graphs/cut_vertex_finding.cpp
+++ Graphs/cut_vertex_finding.cpp
@@ -10,47 +10,55 @@ vector<int> cutpoints;
 
 int timer = 0, tin[NMAX], fuv[NMAX];
 
-void IS_CUTPOINT(int v){
+void IS_CUTPOINT(int v)
+{
     cutpoints.push_back(v);
 }
 
-void dfs(int v, int p = -1){
+void dfs(int v, int p = -1)
+{
     used[v] = true;
     tin[v] = fuv[v] = timer++;
     int children = 0;
-    for(size_t i = 0; i < g[v].size(); i++){
+    for (size_t i = 0; i < g[v].size(); i++)
+    {
         int to = g[v][i];
-        if(to == p)
+        if (to == p)
             continue;
-        if(used[to])
+        if (used[to])
             fuv[v] = min(fuv[v], tin[to]);
-        else{
+        else
+        {
             dfs(to, v);
             fuv[v] = min(fuv[v], fuv[to]);
-            if(fuv[to] >= tin[v] && p != -1)
+            if (fuv[to] >= tin[v] && p != -1)
                 IS_CUTPOINT(v);
             ++children;
         }
     }
-    if(p == -1 && children > 1)
+    if (p == -1 && children > 1)
         IS_CUTPOINT(v);
 }
 
-void find_cut_point(){
-    for(int i = 0; i < NMAX; i++)
+void find_cut_point()
+{
+    for (int i = 0; i < NMAX; i++)
         used[i] = false;
-    for(int i = 0; i < NMAX; i++){
-        if(!used[i])
+    for (int i = 0; i < NMAX; i++)
+    {
+        if (!used[i])
             dfs(i, -1);
     }
     cout << "Cutpoints: ";
-    for(int i = 0; i < cutpoints.size(); i++){
+    for (int i = 0; i < cutpoints.size(); i++)
+    {
         cout << cutpoints[i] << " ";
     }
     cout << endl;
 }
 
-int main(){
+int main()
+{
     g[0] = {1};
     g[1] = {2};
     g[2] = {3};
